1fabbf30c46f78cf2bbea16dfb6db8f88b6cc578
Jakub Cerovsky <jacobcerovsky@gmail.com>
2024-03-22 20:25:09 +0100

    Updates

--- pokedex/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Pokemons } from "./pokemons/Pokemons";
-export { default as Pokemon } from "./pokemon/Pokemon";
-export { default as About } from "./about/About";
-export { default as App } from "./App";
